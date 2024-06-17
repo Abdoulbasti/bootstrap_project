eda833f413b3345830fbb584ef0b0b3ea2ca6509
Abdoulbasti MUKAILA <mukailaabdoulbassit@gmail.com>
2024-06-17 16:58:32 -0700

    fix : bootstrap fixed

--- assets/app.js
+++ assets/app.js
@@ -1,15 +1,19 @@
-import './bootstrap.js';
 /*
  * Welcome to your app's main JavaScript file!
  *
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './styles/app.scss';
+
+//Add bootstrapp libries
 import * as bootstrap from 'bootstrap';
-import 'bootstrap';
+
 import 'bootstrap/js/dist/util';
 import 'bootstrap/js/dist/dropdown';
 
+import './bootstrap';
+import './dashboard';
+import './js/color-modes'
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+console.log('MESSAGER DE CONSOLE POUR TESTE!');
